1c86c1c783a7ce633f7b365c726483cd5222d838
Arleny <arleny.ares@gmail.com>
2024-06-14 17:09:44 +0200

    first commit

---
+++ src/main/java/Pedido.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Pedidos {
+
+    private String idPedido;
+    private String idCliente;
+    private String dataPedido;
+    private ArrayList<>
+}
---
+++ src/main/java/Repartidor.java
@@ -0,0 +1,4 @@
+public class Repartidores {
+
+    private String nombre;
+}
